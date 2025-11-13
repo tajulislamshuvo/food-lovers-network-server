2717ee194ddf1036ff266e8d1f0d69cbccf62884
tajulislamshuvo <tajulislamshuvo7@gmail.com>
2025-11-13 15:11:29 +0600

    last recheck

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const db = client.db('food_db');
     const reviewCollection = db.collection('review');
     const favouriteCollections = db.collection('favourite');
@@ -112,7 +112,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
