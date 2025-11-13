b347204a023210a8456080f40e43dd7a5528d8f0
tajulislamshuvo <tajulislamshuvo7@gmail.com>
2025-11-13 09:41:33 +0600

    add delete api

--- index.js
+++ index.js
@@ -72,6 +72,16 @@ async function run() {
       res.send(result)
     })
 
+    // review delete
+    app.delete('/review/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
